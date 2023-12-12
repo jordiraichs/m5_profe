f00e07959599b94e2fa6b34e55a065438e4e3406
jordi <jraichs@inscaparrella.cat>
2023-12-12 20:27:25 +0100

    comparar

--- src/main/java/local/caparrella/Main.java
+++ src/main/java/local/caparrella/Main.java
@@ -3,6 +3,6 @@ package local.caparrella;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hola a tots!!!");
+        System.out.println("Hola LOCAL!!!");
     }
 }
